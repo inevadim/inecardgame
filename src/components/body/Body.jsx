aa57902888a5846f8017eb6c3db05375ed694f0d
inevadim <78503414+inevadim@users.noreply.github.com>
2022-12-13 22:23:39 +0300

    add 2 block to moove

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,5 +1,27 @@
 import styles from './Body.module.scss';
 
+const MovableItem = () => {
+  return <div className={styles.movableItem}>Перетаскиваемая карточка</div>;
+};
+const FirstColumn = () => {
+  return (
+    <div className={styles.firstColumn}>
+      Колонка 1
+      <MovableItem />
+    </div>
+  );
+};
+
+const SecondColumn = () => {
+  return <div className={styles.secondColumn}>Колонка 2</div>;
+};
 export const Body = () => {
-  return <div className={styles.body}></div>;
+  return (
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <FirstColumn />
+        <SecondColumn />
+      </div>
+    </div>
+  );
 };
