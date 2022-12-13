19991912dcc96e3456c8a16f93f88970a709a3c3
inevadim <78503414+inevadim@users.noreply.github.com>
2022-12-13 23:23:32 +0300

    cleaning code

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,27 +1,5 @@
 import styles from './Body.module.scss';
 
-const MovableItem = () => {
-  return <div className={styles.movableItem}>Перетаскиваемая карточка</div>;
-};
-const FirstColumn = () => {
-  return (
-    <div className={styles.firstColumn}>
-      Колонка 1
-      <MovableItem />
-    </div>
-  );
-};
-
-const SecondColumn = () => {
-  return <div className={styles.secondColumn}>Колонка 2</div>;
-};
 export const Body = () => {
-  return (
-    <div className={styles.body}>
-      <div className={styles.container}>
-        <FirstColumn />
-        <SecondColumn />
-      </div>
-    </div>
-  );
+  return <div className={styles.body}></div>;
 };
